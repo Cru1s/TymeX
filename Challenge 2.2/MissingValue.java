5ab9557db8b9d382caf8abbd715693565f472f84
Long Pham <94373117+Cru1s@users.noreply.github.com>
2025-01-10 13:14:03 +0700

    Add files via upload

--- Challenge 2.2/MissingValue.java
+++ Challenge 2.2/MissingValue.java
@@ -14,7 +14,7 @@ public class MissingValue {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int nums[] = {1,2,3,4,5,6,7,8,10,11,12,13,14,15}; //n = 6
+        int nums[] = {3,7,1,2,6,4}; //n = 6
         findMissingValue(nums.length, nums);
     }
 
